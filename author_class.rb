4e4680279c62a5a17aa1fdeeaefc41586c3896f5
Thi-Ponce <thi.pponce@gmail.com>
2022-05-24 16:07:50 -0300

    fix linters

--- author_class.rb
+++ author_class.rb
@@ -1,15 +1,15 @@
 class Author
-    attr_reader :first_name, :last_name, :items
-  
-    def initialize(first_name, last_name)
-      @id = rand(100_000)
-      @first_name = first_name
-      @last_name = last_name
-      @items = []
-    end
-  
-    def add_item(item)
-      item.author = self
-      @items << item
-    end
+  attr_reader :first_name, :last_name, :items
+
+  def initialize(first_name, last_name)
+    @id = rand(100_000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
   end
+
+  def add_item(item)
+    item.author = self
+    @items << item
+  end
+end
--- game_class.rb
+++ game_class.rb
@@ -8,7 +8,7 @@ class Game < Item
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
-  
+
   def can_be_archived?
     now = Date.today.year
     last_played_at = @last_played_at.year
