c0434d820809d721725a2f3232e9ab2d324c1bd7
yersel500 <yersel500@gmail.com>
2022-05-24 13:04:26 -0500

    Fix linters erros

--- item_class.rb
+++ item_class.rb
@@ -2,7 +2,6 @@ class Item
   attr_accessor :id, :genre, :author, :label, :publish_date, :archived
 
   def initialize(genre, author, label, publish_date)
-
     @id = Random.rand(1..1000)
     @genre = genre
     @author = author
