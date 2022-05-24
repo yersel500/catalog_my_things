1d8eb14bfd509cae6a2cd99c7f827c01b62b3a33
Thi-Ponce <thi.pponce@gmail.com>
2022-05-24 15:58:27 -0300

    Create author and game & implement methods

---
+++ game_class.rb
@@ -0,0 +1,18 @@
+require './item_class'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date, archived)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
+  
+  def can_be_archived?
+    now = Date.today.year
+    last_played_at = @last_played_at.year
+
+    super() && now - last_played_at >= 2
+  end
+end
