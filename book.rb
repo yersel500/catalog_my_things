eb9decb2feb5a18a3064f22acdb360225e2638ed
yersel500 <yersel500@gmail.com>
2022-05-23 19:34:40 -0500

    create Label class

--- book.rb
+++ book.rb
@@ -1,6 +1,8 @@
 require './item_class'
 
 class Book < Item
+  attr_accessor :publisher, :cover_state
+
   def initialize(genre, author, label, publish_date, archived, publisher, cover_state)
     super(genre, author, label, publish_date, archived)
     @publisher = publisher
